7af4395c5c5151bed8c800e57d09454faf29091e
Vad Pol <work.arb19@gmail.com>
2020-01-28 15:00:29 +0300

    money keeper

---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+let start = document.getElementById('start'),
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionaLexpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+    expensesItem = document.getElementsByClassName('.expenses-item'),
+    optionalExpensesBtnApprove = document.querySelector('.optionalexpenses-btn'),
+    countBudgetBtnCalculate = document.querySelector('.count-budget-btn'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses  .optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    accumulation = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value');
+
+    console.log(start);
+    console.log(budgetValue);
+    console.log(dayBudgetValue);
+    console.log(levelValue);
+    console.log(expensesValue);
+    console.log(optionaLexpensesValue);
+    console.log(incomeValue);
+    console.log(monthSavingsValue);
+    console.log(yearSavingsValue);
+    console.log(expensesItem);
+    console.log(optionalExpensesBtnApprove);
+    console.log(countBudgetBtnCalculate);
+    console.log(optionalExpensesItem);
+    console.log(chooseIncome);
+    console.log(accumulation);
+    console.log(sum);
+    console.log(percent);
+    console.log(year);
+    console.log(month);
+    console.log(day);
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
